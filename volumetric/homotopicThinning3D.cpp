c6f527f78d7af4f3c7b55288225a1d5f63f31289
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-05-18 14:49:29 +0200

    Adding graph export options

--- volumetric/homotopicThinning3D.cpp
+++ volumetric/homotopicThinning3D.cpp
@@ -51,6 +51,10 @@
 #include "DGtal/geometry/volumes/distance/SeparableMetricHelper.h"
 #include "DGtal/geometry/volumes/distance/DistanceTransformation.h"
 
+#include <algorithm>                 // for std::for_each
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
 ///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
@@ -82,13 +86,19 @@ int main( int argc, char** argv )
   po::options_description general_opt ( "Allowed options are: " );
   general_opt.add_options()
     ( "help,h", "display this message." )
-    ( "input,i", po::value<std::string>(), "Input vol file." );
+    ( "input,i", po::value<std::string>(), "Input vol file." )
+    ( "output,o", po::value<std::string>(), "Ouput filename without any extension" )
+    ( "vol,v",  "Export the skeleton as a vol file (notyetimplemented)" )
+    ( "graph,g", "Export the skeleton as a weighted graph" )
+    ( "viewer", "Display the skeleton using DGtal::Viewer3D" );
+ 
+
   po::variables_map vm;
   po::store ( po::parse_command_line ( argc, argv, general_opt ), vm );
   po::notify ( vm );
   if ( vm.count ( "help" ) ||argc<=1 )
     {
-      trace.info() << "Illustration of homotopic thinning of a vol file with 3D viewer."<<std::endl
+      trace.info() << "Illustration of guided homotopic thinning of a vol file with 3D viewer."<<std::endl
                    << std::endl << "Basic usage: "<<std::endl
                    << "\thomotopicThinning3d [options] --input <volFileName>"<<std::endl
                    << general_opt << "\n";
@@ -98,8 +108,18 @@ int main( int argc, char** argv )
   //Parse options
   if ( ! ( vm.count ( "input" ) ) ) missingParam ( "--input" );
   std::string filename = vm["input"].as<std::string>();
+ 
+  std::string outputfilename  ;
+  if  ( vm.count ( "output" ) )
+    outputfilename = vm["output"].as<std::string>();
   
-  
+  if  ( ( vm.count ( "vol" )  ||  (vm.count("graph"))) && ( ! vm.count("output")))
+    missingParam ( "--output (mandatory for either vol or graph export)" );
+ 
+  if  (  vm.count ( "output" )  && (!  (vm.count("graph") ||   vm.count("vol"))))
+    missingParam ( "--graph or --vol (mandatory with --output option)" );
+
+  //Starting the process   
   typedef ImageSelector < Z3i::Domain, unsigned char>::Type Image;
   Image image = VolReader<Image>::importVol ( filename );
 
@@ -163,28 +183,96 @@ int main( int argc, char** argv )
   while ( nb_simple != 0 );
   trace.endBlock();
   
-  DigitalSet & S = shape.pointSet();
+  trace.info() << "Skeleton --> "<<shape<<std::endl;
 
-  trace.info() << "Skeleton--> "<<S<<std::endl;
-
-  // Display by using two different list to manage OpenGL transparency.
-  QApplication application(argc,argv);
-  Viewer3D viewer;
-  viewer.setWindowTitle("simpleExample3DViewer");
-  viewer.show();  
-  
-  viewer << SetMode3D( shape_set.className(), "Paving" );
-  viewer << CustomColors3D(Color(25,25,255, 255), Color(25,25,255, 255));
-  viewer << S ; 
-
-  viewer << SetMode3D( shape_set.className(), "PavingTransp" );
-  viewer << CustomColors3D(Color(250, 0,0, 25), Color(250, 0,0, 5));
-  viewer << shape_set;
-
-  viewer<< Viewer3D::updateDisplay;
+  //Graph construction & export
+  if (vm.count("graph"))
+    {
+      trace.beginBlock("Exporting the graph");
+      typedef boost::property<boost::vertex_distance_t, double, 
+                              boost::property<boost::vertex_name_t, std::string> > VertexProperty;
+      
+      typedef boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS
+                                     > Graph;
+      typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+      typedef std::pair<int, int> Edge;
+      typedef std::set < Edge > Edges;
+      std::map < Point, std::size_t > myVertexIndex;
+      std::vector < Point > myCoords;
    
-  return application.exec();
+      Edges edges;
+      
+      const int num_nodes = shape.pointSet().size();
+      Graph G(num_nodes );
+           
+      size_t pos = 0;
+      for(Object26_6::DigitalSet::ConstIterator it = shape.pointSet().begin(),
+            itend = shape.pointSet().end(); it != itend;
+          ++it, ++pos)
+        {
+          myCoords.push_back(*it);
+          myVertexIndex[*it] = pos;
+        }
+      
+      pos = 0;
+      for(Object26_6::DigitalSet::ConstIterator it = shape.pointSet().begin(),
+            itend = shape.pointSet().end(); it != itend; ++it, ++pos)
+        {
+          Object26_6::SmallObject neig = shape.properNeighborhood( * it );
+          for(Object26_6::SmallObject::DigitalSet::ConstIterator itn = neig.pointSet().begin(),
+                itnend=neig.pointSet().end(); itn != itnend; ++itn)
+            add_edge( pos, myVertexIndex.find(*itn)->second , G);        
+        }
+      
+      //Exporting the skeleton
+      std::ofstream out((outputfilename+".graph").c_str());
+      
+      // get the property map for vertex indices
+      typedef boost::property_map<Graph, boost::vertex_index_t>::type IndexMap;
+      IndexMap index = get(boost::vertex_index, G);
+      
+      out << num_nodes<<" "<< boost::num_edges(G)<< std::endl;
+      typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
+      std::pair<vertex_iter, vertex_iter> vp;
+      for (vp = boost::vertices(G); vp.first != vp.second; ++vp.first)
+        out << index[*vp.first] <<  " "
+            << myCoords[index[*vp.first]][0] <<" "
+            << myCoords[index[*vp.first]][1] <<" "
+            << myCoords[index[*vp.first]][2] <<" "
+            << (double) sqrt(resultL2( myCoords[index[*vp.first]] ) ) << std::endl;
+      out << std::endl;
+      // ...
+      
+      boost::graph_traits<Graph>::edge_iterator ei, ei_end;
+      for (tie(ei, ei_end) = boost::edges(G); ei != ei_end; ++ei)
+        out<< index[source(*ei, G)] << " "<< index[target(*ei, G)] << std::endl;
+      // ...
 
+      out.close();
+      trace.endBlock();
+    }
+          
+        
+  // Display by using two different list to manage OpenGL transparency.
+  if (vm.count("viewer"))
+    {
+      QApplication application(argc,argv);
+      Viewer3D viewer;
+      viewer.setWindowTitle("simpleExample3DViewer");
+      viewer.show();  
+      
+      viewer << SetMode3D( shape_set.className(), "Paving" );
+      viewer << CustomColors3D(Color(25,25,255, 255), Color(25,25,255, 255));
+      viewer << shape.pointSet() ; 
+
+      viewer << SetMode3D( shape_set.className(), "PavingTransp" );
+      viewer << CustomColors3D(Color(250, 0,0, 25), Color(250, 0,0, 5));
+      viewer << shape_set;
+      
+      viewer<< Viewer3D::updateDisplay;
+      
+      return application.exec();
+    }
 }
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
